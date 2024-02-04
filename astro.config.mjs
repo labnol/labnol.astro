90675a6b26e2b8af7b0d0658da022d58ba6ba985
Amit Agarwal <amit@labnol.org>
2024-02-04 11:25:30 +0530

    Blog Collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import mdx from '@astrojs/mdx';
+import react from "@astrojs/react";
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.labnol.org',
-  integrations: [mdx(), sitemap(), react(), tailwind()]
+  integrations: [mdx(), sitemap(), react(), tailwind()],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,15 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+const blogCollection = defineCollection({
+  type: 'content',
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(), // Transform string to Date object
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+export const collections = { blog: blogCollection };
