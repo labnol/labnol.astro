e5ebac5ff1ffd7c9b6bd55f4497be20b41c1d2da
Amit Agarwal <amit@labnol.org>
2024-02-04 10:35:10 +0530

    Added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.labnol.org',
-  integrations: [mdx(), sitemap(), react()]
+  integrations: [mdx(), sitemap(), react(), tailwind()]
 });
--- src/components/ReactButton.jsx
+++ src/components/ReactButton.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 export const ReactButton = () => {
-	const [count, setCount] = useState(0)
-	const handleClick = () => setCount(count + 1)
-	return (
-		<button onClick={handleClick}>
-			Clicked {count} times
-		</button>
-	)
-}
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount(count + 1);
+  return (
+    <button
+      onClick={handleClick}
+      className="mt-6 bg-gray-200 p-2 shadow-lg rounded hover:bg-gray-300"
+    >
+      Clicked {count} times
+    </button>
+  );
+};
